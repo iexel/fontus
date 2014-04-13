e201c8f0c92b7e67526a02938cc43227ee7ed6c1
Alexey <alexey.stackoverflow@gmail.com>
2014-04-13 18:48:45 +1000

    license headers have been added

--- fontus-web/src/main/java/net/myproject/security/CustomAuthenticationProvider.java
+++ fontus-web/src/main/java/net/myproject/security/CustomAuthenticationProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 iexel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.myproject.security;
 
 import java.util.ArrayList;
@@ -13,6 +29,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String name = authentication.getName();
 		String password = authentication.getCredentials().toString();
@@ -35,6 +52,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		return null;
 	}
 
+	@Override
 	public boolean supports(Class<?> authentication) {
 		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
